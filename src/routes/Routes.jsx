78df26c8379f18e2ecfe902cb36a696285233f56
HasnatShahriar <shahriarhriday94@gmail.com>
2024-06-06 01:22:28 +0600

    change the route path of admin dashboard

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -87,7 +87,7 @@ const router = createBrowserRouter([
       },
       // admin routes
       {
-        path: "dashboard",
+        path: "",
         element: <AdminDashboard />
       },
       {
